454c40a4801bc4ad506ea5930e13f33aa141afee
rocamonde <juancarlosrocamonde@gmail.com>
2018-09-09 19:46:39 +0200

    Added better structure for commands

--- savannah/core/management.py
+++ savannah/core/management.py
@@ -1,17 +1,20 @@
-# File to include function to run code, to edit settings file, and so much more
 import argparse
 import sys
 from os import environ
 from os.path import join, dirname, realpath
 import subprocess
-from enum import Enum
+from dataclasses import dataclass
 
-from savannah.core.extensions.interpreter import BaseInterpreter
+from savannah.core.interpreter import BaseInterpreter
 from savannah.core.exceptions import MisconfiguredSettings
+
+
+# TODO: print result of command-line calls.
 
 #
 # Recognise commands
 #
+
 
 def execute_from_command_line(argv):
     # Version verifier
@@ -24,42 +27,6 @@ def execute_from_command_line(argv):
     interpreter.run()
 
 
-class cmd(Enum):
-    class Run(BaseInterpreter):
-        def __configure__(self):
-            self.stdparser.add_argument('-sp', '--serverport', nargs='?')
-            self.stdparser.add_argument('-sh', '--serverhost', nargs='?')
-            self.stdparser.add_argument('--uiport', nargs='?')
-            self.stdparser.add_argument('--uihost', nargs='?')
-
-        def execute(self, namespace: argparse.Namespace):
-            self.do_run(**namespace.__dict__)
-
-        def do_run(self, **kwargs):
-            start_savannah(**kwargs)
-
-    class CreateSettings(BaseInterpreter):
-        def __configure__(self):
-            pass
-
-        def execute(self, namespace: argparse.Namespace):
-            self.do_create_settings()
-
-        @staticmethod
-        def do_create_settings():
-            stub_file = join(dirname(realpath(__file__)), 'settings.pyi')
-            p = subprocess.Popen(['python', stub_file], stdout=subprocess.PIPE, bufsize=1)
-            out = p.stdout.read()
-
-            with open(join(environ['SAVANNAH_BASEDIR'], 'settings.json'), "wb") as file:
-                file.write(out)
-
-mapped_commands = {
-            'run': cmd.Run,
-            'create-settings': cmd.CreateSettings,
-        }
-
-
 class CLInterpreter(BaseInterpreter):
 
     def __init__(self, argv=None):
@@ -70,40 +37,92 @@ class CLInterpreter(BaseInterpreter):
         self.stdparser.add_argument('command', nargs='+')
 
     def execute(self, namespace: argparse.Namespace):
-        commands = namespace.__getattr__('command')
-        mapped_commands[commands[0]].value()
+        commands = namespace.__dict__.get('command')
+        subparser: BaseInterpreter = mapped_commands[commands[0]](commands[1:])
+        subparser.run()
 
 
 #
-# Start savannah
+# Define commands
 #
 
-def get_split_pair(host: str, port: str = None):
+class Command(BaseInterpreter):
+    def __init__(self, argv):
+        self.argv = argv
+        super().__init__()
+
+class Run(Command):
+    def __configure__(self):
+        self.stdparser.add_argument('-sh', '--serverhost', nargs='?')
+        self.stdparser.add_argument('-sp', '--serverport', nargs='?')
+        self.stdparser.add_argument('--uihost', nargs='?')
+        self.stdparser.add_argument('--uiport', nargs='?')
+
+    def execute(self, namespace: argparse.Namespace):
+        self.start_savannah(**namespace.__dict__)
+
+    @staticmethod
+    def start_savannah(serverhost: str = None, serverport=None, uihost=None, uiport=None):
+        from savannah.core import settings
+        from savannah.core.units import IOUnit
+
+        # Address fetching
+        try:
+            _iounitaddr = (settings.workflow.server.address.host, int(settings.workflow.server.address.port))
+            _uiaddr = (settings.workflow.localui.address.host, int(settings.workflow.localui.address.port))
+        except TypeError:
+            raise MisconfiguredSettings("Invalid data types in settings.json")
+
+        iounitaddr = _get_split_pair(serverhost, serverport) if serverhost else _iounitaddr
+        uiaddr = _get_split_pair(uihost, uiport) if uihost else _uiaddr
+
+        # Initialize IOUnit
+        iounit = IOUnit(*iounitaddr)
+        iounit.init()
+
+        # Initialize
+
+        if settings.workflow.localui.enabled:
+            pass
+
+
+class CreateSettings(Command):
+    def __configure__(self):
+        pass
+
+    def execute(self, namespace: argparse.Namespace):
+        self.do_create_settings()
+
+    @staticmethod
+    def do_create_settings():
+        stub_file = join(dirname(realpath(__file__)), 'settings.pyi')
+        # Process is run from current executable to ensure that Savannah is installed
+        p = subprocess.Popen([sys.executable, stub_file], stdout=subprocess.PIPE, bufsize=1)
+        out = p.stdout.read()
+
+        with open(join(environ['SAVANNAH_BASEDIR'], 'settings.json'), "wb") as file:
+            file.write(out)
+
+
+@dataclass
+class Commands:
+    run = Run
+    create_settings = CreateSettings
+
+cmd = Commands  # alias
+mapped_commands = {
+            'run': cmd.run,
+            'create-settings': cmd.create_settings,
+        }
+
+
+#
+# Utils
+#
+
+
+def _get_split_pair(host: str, port: str = None):
     try:
         return host.split(':')[0], int(host.split(':')[1])
     except IndexError:
         return host, int(port)
-
-def start_savannah(serverhost: str = None, serverport=None, uihost=None, uiport=None):
-    from savannah.core import settings
-    from savannah.core.units import IOUnit
-
-    # Address fetching
-    try:
-        _iounitaddr = (settings.workflow.server.address.host, int(settings.workflow.server.address.port))
-        _uiaddr = (settings.workflow.localui.address.host, int(settings.workflow.localui.address.port))
-    except TypeError:
-        raise MisconfiguredSettings("Invalid data types in settings.json")
-
-    iounitaddr = get_split_pair(serverhost, serverport) if serverhost else _iounitaddr
-    uiaddr = get_split_pair(uihost, uiport) if uihost else _uiaddr
-
-
-    # Initialize IOUnit
-    iounit = IOUnit(*iounitaddr)
-    iounit.init()
-
-    # Initialize
-
-    if settings.workflow.localui.enabled:
-        pass
