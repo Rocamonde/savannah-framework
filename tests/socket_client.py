07beabff4917bb77fd5125ef134183f4b8832b6a
rocamonde <juancarlosrocamonde@gmail.com>
2018-09-11 00:18:16 +0200

    Minor changes

--- tests/socket_client.py
+++ tests/socket_client.py
@@ -1,7 +1,10 @@
 import unittest
 from savannah.iounit import CPUClient
+from savannah.iounit.sockets import ConnStatus
 
 class SocketConnection(unittest.TestCase):
     def test_receive_updates(self):
         c = CPUClient('127.0.0.1', 5555)
-        print(c.message('updates'))
+        result = c.message('updates')
+        print(result)
+        self.assertEqual(result[0], ConnStatus.CONN_OK)
